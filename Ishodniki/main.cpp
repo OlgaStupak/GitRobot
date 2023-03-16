a2ad0fbe084d379ac79c9ff7349230d6493304bc
OlgaStupak <oastupak@mai.education>
2023-03-16 15:27:40 +0300

    Update main.cpp

--- Ishodniki/main.cpp
+++ Ishodniki/main.cpp
@@ -92,14 +92,14 @@ public:
 		switch (state)
 		{
 		case State_Waiting:
-			if (det->there_is_graffiti())//если граффити есть (не знаю на счет переменной, просто может если есть 1, нет 0)
+			if (det->there_is_graffiti())//если граффити есть 
 			{
 				state = State_Moving;//переходим в состояние движения
 			}
 			break;
 
-		case State_Moving://мы должны как-то получить этот угол, а дальше сравнивать с искомым
-			if (det->detectAngle(5))//проверяем угол: если наш угол больше заданного
+		case State_Moving:
+			if (det->detectAngle(5))//проверяем угол
 			{
 				state = State_Rotate;//переходим в состояние поворота
 			}
@@ -145,6 +145,7 @@ public:
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	Robot r;
 	r.run();
 	return 0;
